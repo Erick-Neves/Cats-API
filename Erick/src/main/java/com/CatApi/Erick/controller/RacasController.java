2d223b497a7e6ae79bd99a412ca0c314f6c226da
Erick Neves <batistasd678@gmail.com>
2020-12-12 17:39:59 -0300

    Criados Model, Repository e Controller com endpoints de acesso

---
+++ Erick/src/main/java/com/CatApi/Erick/controller/RacasController.java
@@ -0,0 +1,51 @@
+package com.CatApi.Erick.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.CatApi.Erick.model.RacasModel;
+import com.CatApi.Erick.repository.RacasRepository;
+
+@RestController
+@RequestMapping("/racas")
+@CrossOrigin("*")
+public class RacasController {
+	
+	@Autowired
+	private RacasRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<RacasModel>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<RacasModel> getById(@PathVariable long id){
+		return repository.findById(id)
+				.map(response -> ResponseEntity.ok(response))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<RacasModel>> getAllByNome(@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@GetMapping("/origem/{origem}")
+	public ResponseEntity<List<RacasModel>> getAllByOrigem(@PathVariable String origem){
+		return ResponseEntity.ok(repository.findAllByOrigemContainingIgnoreCase(origem));
+	}
+	
+	@GetMapping("/temperamento/{temperamento}")
+	public ResponseEntity<List<RacasModel>> getAllByTemperamento(@PathVariable String temperamento){
+		return ResponseEntity.ok(repository.findAllByTemperamentoContainingIgnoreCase(temperamento));
+	}
+	
+	
+}
---
+++ Erick/src/main/java/com/CatApi/Erick/model/RacasModel.java
@@ -0,0 +1,105 @@
+package com.CatApi.Erick.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_racas")
+public class RacasModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private String origem;
+	
+	@NotNull
+	private String temperamento;
+	
+	@NotNull
+	private String descricao;
+	
+	@NotNull
+	private String img_1;
+	
+	@NotNull
+	private String img_2;
+	
+	@NotNull
+	private String img_3;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(String origem) {
+		this.origem = origem;
+	}
+
+	public String getTemperamento() {
+		return temperamento;
+	}
+
+	public void setTemperamento(String temperamento) {
+		this.temperamento = temperamento;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getImg_1() {
+		return img_1;
+	}
+
+	public void setImg_1(String img_1) {
+		this.img_1 = img_1;
+	}
+
+	public String getImg_2() {
+		return img_2;
+	}
+
+	public void setImg_2(String img_2) {
+		this.img_2 = img_2;
+	}
+
+	public String getImg_3() {
+		return img_3;
+	}
+
+	public void setImg_3(String img_3) {
+		this.img_3 = img_3;
+	}
+	
+	
+
+}
